5d8555ff5e16c7c38c0db2f46dc3981ba8445b81
Roger Smith <rogsmith@gmail.com>
2013-08-15 12:09:14 -0400

    tickets by milestone

--- lib/assemblabla/ticket.rb
+++ lib/assemblabla/ticket.rb
@@ -1,4 +1,9 @@
 module Assembla
   class Ticket < Assembla::Base
+
+
+  	def self.by_milestone(space_id, milestone_id)
+  		self.find(:all, :from => "spaces/#{space_id}/tickets/milestone/#{milestone}")
+  	end
   end
 end
